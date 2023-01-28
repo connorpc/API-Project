28c1dec8367a5f8efe77e76a3a5b43b0f0cf1316
connor <phelpscg@icloud.com>
2023-01-28 14:06:39 -0800

    added user input validation on signup endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,14 +6,38 @@ const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation.js');
+
 const router = express.Router();
+
+// validate signup middleware
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+    .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be at least 6 characters'),
+  handleValidationErrors
+];
 
 // post /api/users route
 // inside the (async) route handler will be a call to the User static method
 // signup(). if a new user is successfully created, call setTokenCookie and return
 // a json response with the user information (scope?). if creation is unsuccessful,
 // then a sequelize validation error will be passed onto the next err handling mware
-router.post('/', async (req, res) => {
+router.post('/', validateSignup, async (req, res) => {
       const { email, password, username } = req.body;
       const user = await User.signup({ username, email, password });
 
