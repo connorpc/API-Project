dbbe202258861774f99a5ed53ca9fe6c5b094db4
connor <phelpscg@icloud.com>
2023-01-29 23:47:27 -0800

    debugging on main lol2

--- backend/db/migrations/20230128222102-add_firstName_lastName.js
+++ backend/db/migrations/20230128222102-add_firstName_lastName.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+    options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -11,11 +16,11 @@ module.exports = {
      */
     await queryInterface.addColumn('Users', 'firstName', {
       type: Sequelize.STRING,
-    });
+    }, options);
 
     await queryInterface.addColumn('Users', 'lastName', {
       type: Sequelize.STRING,
-    });
+    }, options);
   },
 
   async down (queryInterface, Sequelize) {
